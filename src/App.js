9ace13beafbb9f5e60ca01fd5628c0293874f703
Moe Bachrouch <54571275+moebachrouch@users.noreply.github.com>
2020-09-06 14:38:32 -0400

    deployed website

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
   this.state = {
     title: 'Moe Bachrouch',
     headerLinks: [
-      {title: 'Home', path:'/portfolio'},
+      {title: 'Home', path:'/'},
       {title: 'About', path:'/about'},
       {title: 'Contact', path:'/contact'}
     ],
@@ -50,14 +50,14 @@ class App extends React.Component {
           <Navbar.Toggle className="border-0" aria-controls="navbar-toggle"/>
           <Navbar.Collapse id = "navbar-toggle">
             <Nav className="ml-auto">
-              <Link className="nav-link" to="/portfolio">Home</Link>
+              <Link className="nav-link" to="/">Home</Link>
               <Link className="nav-link" to="/about">About</Link>
               <Link className="nav-link" to="/contact">Contact</Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
 
-        <Route path="/portfolio" exact render={ () => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text}/>}/>
+        <Route path="/" exact render={ () => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text}/>}/>
         <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
           <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
 
