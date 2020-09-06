b51570f3b5384f9568d74e4c0a3a8369d2048a6e
Moe Bachrouch <54571275+moebachrouch@users.noreply.github.com>
2020-09-06 18:38:24 -0400

    changed flavicon

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -7,19 +7,19 @@ function AboutPage(props) {
         <div>
             <Hero title={props.title}/>
             <Content>
-            <p>Welcome,</p>
+<p>Welcome,</p>
 
-<p>My name is Moe, and I want to make some noise in the Software industry.</p>
+<p>My name is Moe and I'm a software developer.</p>
 
-<p>After completing a Bachelor's degree in Science, I decided to study Software Engineering at the University of Ottawa. I enjoy coding and learning new Software tools. The main languages I work with are Python, Java, and JavaScript.</p>
+<p>I have a Bachelor's degree in science, and I am now studying Software Engineering at the University of Ottawa. I enjoy coding and learning new Software tools. I work with multiple languages including Python, Java, and JavaScript.</p>
 
-<p>Computers fascinate me, so I try to learn as much as I can about them. I find both Hardware and Software aspects interesting, so I experiment with Arduinos sometimes. Web development is also fun for me, so I play around with HTML, CSS, and JavaScript when I can.</p>
+<p>Computers fascinate me, so I'm always learning new computer skills. I find both Hardware and Software interesting. Sometimes, I experiment with Arduinos. Other times, I do Web development using HTML, CSS, and JavaScript.</p>
 
-<p>When I'm not coding, I like to produce music using Logic Pro X, play with 3D printers and 3D design tools, or go outside for a run. I'm a pretty social guy and I like people, so I try to reconnect with friends on my free time.</p>
+<p>When I'm not coding, I like to produce music using Logic Pro X, play with 3D printers, or go outside for a run. I'm a social guy and I like people, so I try to reconnect with friends on my free time.</p>
 
-<p>I'm always open to new opportunities. You can reach me at <a href="mailto: moe.bachrouch@gmail.com">moe.bachrouch@gmail.com</a> for any inquiries.</p>
+<p>I'm always open to opportunities. Feel free to reach me at <a href="mailto: moe.bachrouch@gmail.com">moe.bachrouch@gmail.com</a> for any inquiries.</p>
 
-<p>You can also view my latest <a rel= "noopener noreferrer" target="_blank" href="https://drive.google.com/file/d/1Zd_RQ8mfPS9eTY8O_3Y0too1oS2nCsUZ/view?usp=sharing">resume</a>.</p>
+<p>You can view my latest resume <a rel= "noopener noreferrer" target="_blank" href="https://drive.google.com/file/d/1Zd_RQ8mfPS9eTY8O_3Y0too1oS2nCsUZ/view?usp=sharing">here</a>.</p>
             </Content>
         </div>
     );
