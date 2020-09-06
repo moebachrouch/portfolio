fdd42e1ccd499f7b9f6eba8cddb5f8c73599746f
Moe Bachrouch <54571275+moebachrouch@users.noreply.github.com>
2020-09-06 19:19:36 -0400

    changed flavicon

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ class App extends React.Component {
 
         <Route path="/" exact render={ () => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text}/>}/>
         <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
-          <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
+        <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
 
         <Footer>
 
