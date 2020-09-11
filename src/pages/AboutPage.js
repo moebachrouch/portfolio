07b8bccbefd799c7943cb5933127367333c055df
Moe Bachrouch <54571275+moebachrouch@users.noreply.github.com>
2020-09-11 00:25:33 -0400

    fixed error in about me

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -7,7 +7,7 @@ function AboutPage(props) {
         <div>
             <Hero title={props.title}/>
             <Content>
-<h3>Welcome,</h3>
+<p>Welcome,</p>
 
 <p>My name is Moe and I'm a software developer.</p>
 
